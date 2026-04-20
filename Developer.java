091527e663f1b4533e928bdd67e1c97639b25320
Shahad <shahadalshehri05@gmail.com>
2026-04-20 18:58:37 +0300

    Update Developer.java

--- Developer.java
+++ Developer.java
@@ -1,11 +1,3 @@
-/*
-Shahad Rafi Alshehri
-2306119
-ICS
-Assignment 1
- */
-package assignment2;
-
 public class Developer extends Employee {
 //constructor
     public Developer(String name, int id, double salary) {
