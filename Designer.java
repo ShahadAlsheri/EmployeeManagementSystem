5c78eeb4a67d17e1e58a05988204f96f836d0fb5
Shahad <shahadalshehri05@gmail.com>
2026-04-20 18:58:18 +0300

    Update Designer.java

--- Designer.java
+++ Designer.java
@@ -1,10 +1,3 @@
-/*
-Shahad Rafi Alshehri
-2306119
-ICS
-Assignment 2
- */
-package assignment2;
 public class Designer extends Employee{
 //constructor    
     public Designer(String name, int id, double salary){
